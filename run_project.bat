ab7b9aad9e5c566be785f1d7b237fd81c6938300
Harley Townsend <harleytownsend.wrk@gmail.com>
2026-03-04 19:25:23 +0000

    Update run_project.bat

--- run_project.bat
+++ run_project.bat
@@ -1,5 +1,5 @@
 @echo off
 
 start cmd /k "python C:\Users\Harley\Documents\GitHub\HandTrackingProject\hand_tracking_project\hand_tracking.py"
-timeout /t 2 >nul
-start "" "C:\Users\Harley\Documents\GitHub\HandTrackingProject\handtrackingproject\.godot"
+timeout /t 4 >nul
+start "" "C:\Users\Harley\scoop\apps\godot\current\godot.exe" --path "C:\Users\Harley\Documents\GitHub\HandTrackingProject\handtrackingproject"
